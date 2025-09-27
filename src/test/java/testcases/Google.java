d24034ad547369e204f5b9bcf578363b9152bb7b
naveen180944 <sn15208m044@gmail.com>
2025-09-27 09:31:14 +0530

    Initial commit from Eclipse Maven project

---
+++ src/test/java/testcases/Google.java
@@ -0,0 +1,21 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Google {
+
+	WebDriver driver;
+	
+	@Test
+	public void test() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+			
+	}
+}
